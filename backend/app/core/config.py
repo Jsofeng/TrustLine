d35ff207c6807eaeace02f563ae7d879e1c845d8
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-31 20:38:00 -0400

    feat: .env config

---
+++ backend/app/core/config.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+"""
+Takes .env URL's & gets converted into 
+
+settings.database_url
+settings.redis_url
+"""
+
+class Settings(BaseSettings): 
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore") #ignore extra variables
+
+    #environment variables are case-insensitive by default.
+    database_url: str
+    migration_database_url: str 
+    test_database_url: str 
+    redis_url: str
+
+@lru_cache #a cache for retrieving settings so doesn't have to be recalled hundreds of times
+def get_settings() -> Settings:
+    return Settings()
+
+
+
