fcbdb9c9121142c7abfbaa227da35b46d089ac9e
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-08-01 00:01:21 -0400

    fixed AsyncIterator

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -7,6 +7,6 @@ from app.core.config import get_settings
 engine = create_async_engine(get_settings().database_url, pool_pre_ping=True) #pool_pre_ping -> checks if this connection still alive before using, if it's dead creates a new one
 SessionLocal = async_sessionmaker(engine=engine, expire_on_commit=False) # expire_on_commit allows the object to keep its values
 
-async def get_db_session() -> AsyncIterator(AsyncSession):
+async def get_db_session() -> AsyncIterator[AsyncSession]:
     async with SessionLocal() as session:
         yield session
